2bccb9a76926f619b7ba2a7c8011c54f3ab2c5ad
Daniel <83504117+ELODollZ@users.noreply.github.com>
2023-12-07 18:36:24 +0100

    Update PatientIndexing.py

--- Scripts/PatientIndexing.py
+++ Scripts/PatientIndexing.py
@@ -127,7 +127,7 @@ def interActiveMenu(conn, cursor, TableNamed, PileListeFormat):
             StoreData = GetDataFormPatientsListe(cursor, PatientInfo)
             if StoreData:
                 print("Search Result:")
-                if StoreData and 'PileListeData' in StoreData:
+                if 'PileListeData' in StoreData:
                     print(StoreData)
                     print("\nPilListe Details:")
                     for PileData in StoreData['PileListeData']:
