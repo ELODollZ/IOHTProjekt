0e4ae159fd0eec9337a9336afd2350cbf3b376ea
Daniel <83504117+ELODollZ@users.noreply.github.com>
2023-12-13 11:03:22 +0100

    Update app.py

--- Hjemmeside/app.py
+++ Hjemmeside/app.py
@@ -57,5 +57,5 @@ ThreadDBC.start()
 
 #Host webpage onto network
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=2901, debug=True)
+    socketio.run(app, host={Conf.RPIServerAddress}, port={Conf.RPIPortNumber}, debug=True)
 
