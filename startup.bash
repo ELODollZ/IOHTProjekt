854c781e6927fcd6ab9e64e0e0287078b5c0c33e
ELODollZ <DanielDKN29@gmail.com>
2023-12-17 13:49:28 +0100

    Screen session maker

--- startup.bash
+++ startup.bash
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 # Arthur: NyboMønster
 
+venvDir="/home/Gruppe2PI/Projekts/IOHTProjekt/FlaskEnviroment/FlaskEnviroment"
+
 if screen -list | grep -q "flaskInterActiveMenu"; then
 	screen -S flaskInterActiveMenu -X quit
 else
 	echo 'no session in that name'
 fi
 
-screen -S flaskInterActiveMenu -dm bash -c 'export FLASK_APP=app.py; export FLASK_RUN_HOST=192.168.29.116; export FLASK_RUN_PORT=2916; flask run'
+screen -S flaskInterActiveMenu -dm bash -c "source $venvDir/bin/activate && export FLASK_APP=app.py; export FLASK_RUN_HOST=192.168.29.116; export FLASK_RUN_PORT=2916; flask run"
+
+screen r flaskInterActiveMenu
+
